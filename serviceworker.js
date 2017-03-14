40d16d9cd30ee6a673ccf80029ec8527d8a29d46
JodiePebble <maddox.jodie@gmail.com>
2017-03-14 15:40:12 +0000

    return service worker request

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response(
-        'Welcome to the our service worker test \n'+        
-        'There seems to be a problem with your connection.\n'+        
-        'We look forward to telling you about Paddy as soon as you are online'
-      );
-    })
-  );
-});
+  if (event.request.url.indexOf('paddy.jpg') !== -1) {
+    event.respondWith(
+      fetch('gerbil.jpg')
+    );
+  }
+})
